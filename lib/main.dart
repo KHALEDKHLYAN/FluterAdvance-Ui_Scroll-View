7f20f1dbfc61c63207b47a0eb6f5fed071e15dc4
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-07-02 11:02:25 +0300

    samples

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,30 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-void main() => runApp(MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Hello Khaled'),
-        centerTitle: true,
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        BlueBox(),
+        BlueBox(),
+        BlueBox(),
+      ],
+    );
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
       ),
-    )));
+    );
+  }
+}
